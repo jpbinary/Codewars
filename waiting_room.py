c8b54deca4abec750d740552ea79b57250718d3c
jpbinary <jp@iskyy.com>
2015-10-18 19:35:02 -0500

    updated waiting_room.py

--- waiting_room.py
+++ waiting_room.py
@@ -1,22 +1,40 @@
 NUMBER_OF_CHAIRS = 10
 
+def find_chair_as_far_from_others():
+    pass
+
+def find_closest_empty_chair(empty_chairs_distance_dict):
+    closest_empty_chair = -1
+    for key in empty_chairs_distance_dict:
+        print key, empty_chairs_distance_dict[key] #print key, value
+        if closest_empty_chair == -1:
+            closest_empty_chair = key
+        elif closest_empty_chair > key:
+            closest_empty_chair = key
+    print closest_empty_chair, empty_chairs_distance_dict[closest_empty_chair]
+    return closest_empty_chair
 
 def choose_chair(choose_from_chair_patients_list):
+    distance_to_exit_dict = {}
     print choose_from_chair_patients_list
     number_of_chairs = len(choose_from_chair_patients_list)
     print number_of_chairs
 
-    distance_to_other_people_list = []
     for each_chair in choose_from_chair_patients_list:
 
         if each_chair[1] == 0:
             print str(each_chair[0]) + " empty chair"
+            distance_to_exit = each_chair[0] - 1
+            distance_to_exit_dict[each_chair[0]] = distance_to_exit
+            print "distance_to_exit " + str(distance_to_exit)
+            empty_chair_number = each_chair[0]
 
         else:
             print str(each_chair[0]) + " occupied chair"
             # for each occupied chair, find the furthest chair from this patient
 
-
+    print distance_to_exit_dict
+    find_closest_empty_chair(distance_to_exit_dict)
 
 def last_chair(n_chairs):
     # make sure more than 2 chairs
