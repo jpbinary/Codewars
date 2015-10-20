49e8ac62d050789993d17b2d8abe2d0b7bfb82d9
jpbinary <jp@iskyy.com>
2015-10-19 19:31:30 -0500

    updated

--- waiting_room2.py
+++ waiting_room2.py
@@ -1,9 +1,5 @@
-'''def calc_distance_from_potential_chair_to_closest_occupied_chairs(seats_taken_calc, potential_chairs_and_distance_to_closest_occupied_chairs_calc):
-    print seats_taken_calc
-    print potential_chairs_and_distance_to_closest_occupied_chairs_calc
-    pass
-'''
-def calc_distance_to_chairs(potential_chair_cdc, first_chair_number, second_chair_number):
+
+def calc_distance_to_occupied_chairs(potential_chair_cdc, first_chair_number, second_chair_number):
     print potential_chair_cdc
     print first_chair_number
     distance_to_first_chair = abs(potential_chair_cdc-first_chair_number)
@@ -15,11 +11,12 @@ def calc_distance_to_chairs(potential_chair_cdc, first_chair_number, second_chai
 def calc_potential_chairs(seats_taken_c):
     '''calculate what potential chairs the next person should sit in.
      based on distance to other occupied chairs.'''
+
     seats_taken_c_index_location = 0
     # need dictionary to hold potential chair as key, and a 2 item list with the
     # distance from the occupied chairs to the potential chair
-
     potential_chairs_and_distance_to_closest_occupied_chairs = {}
+
     while seats_taken_c_index_location < len(seats_taken_c)-1:
         # take value of list item at index plus value of list item at index+1
         # and divide them by 2. This is the mid-way point between the two chairs.
@@ -29,7 +26,7 @@ def calc_potential_chairs(seats_taken_c):
         # equal to the potential chair, and a value equal to the distance to the 2
         # closest occupied chairs combined
         potential_chairs_and_distance_to_closest_occupied_chairs[potential_chair] = \
-            calc_distance_to_chairs(potential_chair,
+            calc_distance_to_occupied_chairs(potential_chair,
                                     seats_taken_c[seats_taken_c_index_location],
                                     seats_taken_c[seats_taken_c_index_location + 1])
         seats_taken_c_index_location += 1
@@ -61,19 +58,33 @@ def init_chairs_people(number_of_people):
 
 
 def last_chair(n):
-    chairs_people = init_chairs_people(n)
-    print chairs_people
-    chairs_people = assign_first_and_second_person_chairs(chairs_people)
-    print chairs_people
-    seats_taken = what_seats_are_taken(chairs_people)
-    print seats_taken
-    potential_chairs = calc_potential_chairs(seats_taken)
-    print potential_chairs
+    if n <= 2:
+        print "please enter a value greater than 2"
+    else:
+        chairs_people = init_chairs_people(n)
+        print chairs_people
+        # 1st and 2nd person enter
+        chairs_people = assign_first_and_second_person_chairs(chairs_people)
+        print chairs_people
+        seats_taken = what_seats_are_taken(chairs_people)
+        print seats_taken
 
-    return
+        # 3rd person enters
+        potential_chairs = calc_potential_chairs(seats_taken)
+        print 'potential chairs: ', potential_chairs
+
+
+        for potential_chair, potential_chair_distance in potential_chairs.iteritems():
+            print potential_chair, potential_chair_distance
+        if len(potential_chairs) == 1:
+            chairs_people.update(potential_chairs)
+        print chairs_people
+
+
+        return
 
 
 
-
-last_chair(10)
-last_chair(5)
+last_chair(2)
+#last_chair(10)
+#last_chair(5)
